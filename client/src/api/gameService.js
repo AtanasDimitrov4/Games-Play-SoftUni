793ec67f91a2002ac43d26824a5c04c81fd5362f
Atanas Dimitrov4 <ataldi@abv.bg>
2025-03-13 16:38:24 +0200

    Get all games

--- client/src/api/gameService.js
+++ client/src/api/gameService.js
@@ -4,8 +4,13 @@ import requester from "../utils/requester";
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export default {
-    getAll() {
-        return requester.get(baseUrl)
+    async getAll() {
+       const result = await requester.get(baseUrl);
+
+       const games = Object.values(result);
+
+       return games;
+
     },
     create(gameData) {
        return requester.post(baseUrl, gameData);
