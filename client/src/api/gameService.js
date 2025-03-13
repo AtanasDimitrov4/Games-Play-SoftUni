89cff7ba8f2036d7c29a9bd07c2468993a7555f2
Atanas Dimitrov4 <ataldi@abv.bg>
2025-03-13 13:05:43 +0200

    fix requester to have axios functionality
    add some permanent data in server

--- client/src/api/gameService.js
+++ client/src/api/gameService.js
@@ -1,17 +1,13 @@
+import requester from "../utils/requester";
+
+
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 export default {
-    async create(gameData) {
-        const response = await fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(gameData)
-        });
-
-        const result = await response.json();
-
-        return result;
+    getAll() {
+        return requester.get(baseUrl)
+    },
+    create(gameData) {
+       return requester.post(baseUrl, gameData);
     }
-}
+};
--- client/src/components/create-game/CreateGame.jsx
+++ client/src/components/create-game/CreateGame.jsx
@@ -1,23 +1,35 @@
+import { useNavigate } from 'react-router'
+import gameService from "../../api/gameService";
+
 export default function CreateGame() {
-    return(
+    const navigate = useNavigate()
+    const submitAction = async (formData) => {
+        const gameData = Object.fromEntries(formData);
+
+        await gameService.create(gameData);
+
+        navigate('/games');
+    };
+
+    return (
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" action={submitAction}>
                 <div className="container">
 
                     <h1>Create Game</h1>
-                    <label htlmFor="leg-title">Legendary title:</label>
+                    <label htmlFor="leg-title">Legendary title:</label>
                     <input type="text" id="title" name="title" placeholder="Enter game title..." />
 
-                    <label htlmFor="category">Category:</label>
+                    <label htmlFor="category">Category:</label>
                     <input type="text" id="category" name="category" placeholder="Enter game category..." />
 
-                    <label htlmFor="levels">MaxLevel:</label>
+                    <label htmlFor="levels">MaxLevel:</label>
                     <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
 
-                    <label htlmFor="game-img">Image:</label>
+                    <label htmlFor="game-img">Image:</label>
                     <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
 
-                    <label htlmFor="summary">Summary:</label>
+                    <label htmlFor="summary">Summary:</label>
                     <textarea name="summary" id="summary"></textarea>
                     <input className="btn submit" type="submit" value="Create Game" />
                 </div>
