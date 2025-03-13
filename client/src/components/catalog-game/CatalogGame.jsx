3654229d07b227199f68337644d6ab4ac4b98835
Atanas Dimitrov4 <ataldi@abv.bg>
2025-03-13 17:16:15 +0200

    List games in catalog page

--- client/src/components/catalog-game/CatalogGame.jsx
+++ client/src/components/catalog-game/CatalogGame.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import gameService from "../../api/gameService";
+import CatalogGameItem from "./catalog-game-item/CatlogGameItem";
 
 export default function CatalogGame() {
     const [games, setGames] = useState([]);
@@ -14,33 +15,8 @@ export default function CatalogGame() {
     return (
         <section id="catalog-page">
         <h1>All Games</h1>
-        {/*<!-- Display div: with information about every game (if any) -->*/}
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="/images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Cover Fire</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="/images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>Zombie lang</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-
-        </div>
-        <div className="allGames">
-            <div className="allGames-info">
-                <img src="/images/avatar-1.jpg" />
-                <h6>Action</h6>
-                <h2>MineCraft</h2>
-                <a href="#" className="details-button">Details</a>
-            </div>
-        </div>
+        {games.map(game =>  <CatalogGameItem key={game._id} {...game} />)}
+       
 
         {/*<!-- Display paragraph: If there is no games  -->*/}
         <h3 className="no-articles">No articles yet</h3>
